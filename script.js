71e786909a387c223c9d4237397abb8d66f8fa9f
monirwebcode <bongohut.net@gmail.com>
2024-01-06 20:08:04 +0600

    steps

---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+
+
+function checkAge(){
+  age=parseInt(document.getElementById("input_age").value);
+
+  
+
+  let child_card =document.getElementById('child_card');
+  let adult_card =document.getElementById('adult_card');
+
+  if (age<18){
+    child_card.style.display='block';
+    adult_card.style.display='none';
+  }
+  if(age>18){
+    adult_card.style.display='block';
+    child_card.style.display='none';
+  }
+}
